fb29e4d3cf444f6739afd991eb35d44a9963db1d
Evgeny Lukoyanov <e.lukoyanov@rambler-co.ru>
2018-08-09 11:20:38 +0300

    build: update node-sass dependency, fix code style

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-
-var fs    = require('fs');
-var path  = require('path');
-var types = require('node-sass').types;
+var fs = require("fs");
+var path = require("path");
+var types = require("node-sass").types;
 
 var svg = function(buffer) {
-    var svg = buffer.toString()
-        .replace(/\n/g, '')
-        .replace(/\r/g, '')
-        .replace(/\#/g, '%23')
-        .replace(/\"/g, "'");
+  var svg = buffer
+    .toString()
+    .replace(/\n/g, "")
+    .replace(/\r/g, "")
+    .replace(/\#/g, "%23")
+    .replace(/\"/g, "'");
 
-    return '"data:image/svg+xml;utf8,' + svg + '"';
+  return '"data:image/svg+xml;utf8,' + svg + '"';
 };
 
 var img = function(buffer, ext) {
-    return '"data:image/' + ext + ';base64,' + buffer.toString('base64') + '"';
+  return '"data:image/' + ext + ";base64," + buffer.toString("base64") + '"';
 };
 
 module.exports = function(options) {
-    options = options || {};
+  options = options || {};
 
-    var base = options.base || process.cwd();
-    return {
-        'inline-image($file)': function(file) {
-            // we want to file relative to the base
-            var relativePath = './' + file.getValue();
-            var filePath = path.resolve(base, relativePath);
+  var base = options.base || process.cwd();
+  return {
+    "inline-image($file)": function(file) {
+      // we want to file relative to the base
+      var relativePath = "./" + file.getValue();
+      var filePath = path.resolve(base, relativePath);
 
-            // get the file ext
-            var ext = filePath.split('.').pop();
+      // get the file ext
+      var ext = filePath.split(".").pop();
 
-            // read the file
-            var data = fs.readFileSync(filePath);
+      // read the file
+      var data = fs.readFileSync(filePath);
 
-            var buffer = new Buffer(data);
+      var buffer = new Buffer(data);
 
-            if(ext === 'svg'){
-              if(options.useBase64ForSVG){
-                return types.String(img(buffer, "svg+xml"));
-              }else{
-                return types.String(svg(buffer, ext));
-              }
-            }else{
-              return types.String(img(buffer, ext));
-            }
+      if (ext === "svg") {
+        if (options.useBase64ForSVG) {
+          return types.String(img(buffer, "svg+xml"));
+        } else {
+          return types.String(svg(buffer, ext));
         }
-    };
+      } else {
+        return types.String(img(buffer, ext));
+      }
+    }
+  };
 };
